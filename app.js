8b07d2a2223e80408ded488e30e8b09fdda43774
Mark Lewis <marklewis@Mark-MBP-14-2023.fritz.box>
2023-12-06 13:10:45 +0100

    feature: file upload bitches

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const socket = require("socket.io");
 
 // App setup
-const PORT = 5000;
+const PORT = 9000;
 const app = express();
 const server = app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}`);
@@ -27,8 +27,26 @@ const activeUsers = new Set();
 
 io.on("connection", function (socket) {
 
-  socket.on("chat message", function (data) {
-    io.emit("chat message", data);
+  // handle text
+  socket.on("text-message", function (data) {
+    io.emit("text-message", data);
+  });
+
+  // handle files
+  socket.on('file-message', function(name, buffer) {
+    var fs = require('fs');
+
+    //path to store uploaded files (NOTE: presumed you have created the folders)
+    var fileName = __dirname + '/uploads/' + name;
+    fs.open(fileName, 'a', 0755, function(err, fd) {
+        if (err) throw err;
+
+        fs.write(fd, buffer, null, 'Binary', function(err, written, buff) {
+            fs.close(fd, function() {
+                console.log('File saved successful!');
+            });
+        })
+    });
   });
   
 });
--- public/main.js
+++ public/main.js
@@ -1,20 +1,64 @@
 const socket = io();
-
 const inputField = document.querySelector(".message_form__input");
 const messageForm = document.querySelector(".message_form");
 
+var filesUpload = null;
+var file = null;
+
+if (window.File && window.FileReader && window.FileList) {
+  //HTML5 File API ready
+  init();
+} else {
+  //TODO
+}
+
+function init() {
+  // FILE UPLOAD FEATURE
+  filesUpload = document.getElementById('input-files');
+  filesUpload.addEventListener('change', fileHandler, false);
+}
+
 messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  // send file if selected
+  sendFile();
+
   if (!inputField.value) {
     return;
   }
 
-  socket.emit("chat message", {
+  socket.emit("text-message", {
     message: inputField.value
   });
   inputField.value = "";
 });
 
-socket.on("chat message", function (data) {
+socket.on("text-message", function (data) {
     socket.emit({ message: data.message });
 });
+
+function fileHandler(e) {
+  var files = e.target.files || e.dataTransfer.files;
+
+  if (files) {
+      //send only the first one
+      file = files[0];
+  }
+}
+
+function sendFile() {
+  if (file) {
+    //read the file content and prepare to send it
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+        console.log('Sending file...');
+        //get all content
+        var buffer = e.target.result;
+        //send the content via socket
+        socket.emit('file-message', file.name, buffer);
+    };
+    reader.readAsBinaryString(file);
+  }
+}
