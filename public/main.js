9dfaccc239e69a64315b4b9b3db53b21c5e3fe03
Pete <pete@petehaughie.com>
2023-12-06 12:28:42 +0100

    Latest code changes, breaks and all

--- public/main.js
+++ public/main.js
@@ -1,13 +1,7 @@
 const socket = io();
 
-const inboxPeople = document.querySelector(".inbox__people");
 const inputField = document.querySelector(".message_form__input");
-const inputFile = document.querySelector(".message_form__input");
 const messageForm = document.querySelector(".message_form");
-const messageBox = document.querySelector(".messages__history");
-const fallback = document.querySelector(".fallback");
-const formidable = require('formidable');
-const fs = require('fs');
 
 messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -22,5 +16,5 @@ messageForm.addEventListener("submit", (e) => {
 });
 
 socket.on("chat message", function (data) {
-  socket.emit({ message: data.message });
+    socket.emit({ message: data.message });
 });
